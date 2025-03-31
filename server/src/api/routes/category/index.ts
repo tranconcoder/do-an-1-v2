718cf7cf764dbaf0003c70bae6adcb4acae230d0
tvconss <tranconcoder@gmail.com>
2025-03-31 07:53:06 +0000

    init: category careate route

--- server/src/api/routes/category/index.ts
+++ server/src/api/routes/category/index.ts
@@ -11,21 +11,23 @@ import { updateCategory } from '@/validations/joi/category.joi.js';
 import { Router } from 'express';
 
 const categoryRoute = Router();
-const categoryUpdateAnyRoute = Router();
+const categoryRouteValidate = Router();
 
 categoryRoute.get('/', catchError(categoryController.getAlLCategories));
 
 /* ---------------------------------------------------------- */
 /*                          Validated                         */
 /* ---------------------------------------------------------- */
-categoryRoute.use(
-    authenticate,
-    authorization('updateAny', Resources.CATEGORY),
-    categoryUpdateAnyRoute
-);
+categoryRoute.use(authenticate, categoryRouteValidate);
 
-categoryUpdateAnyRoute.put(
+/* ---------------------------------------------------------- */
+/*                           Create                           */
+/* ---------------------------------------------------------- */
+categoryRouteValidate.post('/create', authorization('createAny', Resources.CATEGORY));
+
+categoryRouteValidate.put(
     '/:_id',
+    authorization('updateAny', Resources.CATEGORY),
     validateRequestParams(paramsId('_id')),
     validateRequestBody(updateCategory)
 );
